3b205c204bcdf292f6ef1f4a582af331e2d8c9f9
Paulius <pastra18@student.aau.dk>
2020-11-15 15:38:41 +0100

    added cors support on port 8000

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,42 @@ import express, { Express } from "express"
 import mongoose from "mongoose"
 import diveRoutes from "./dive/routes"
 import userRoutes from "./user/routes"
+import cors from 'cors';
+import CompositionRoot from "./CompositionRoot";
 
 const app: Express = express()
 
-const PORT: string | number = process.env.PORT || 3000
+const PORT = 3000
 
+//options for cors midddleware
+const cors_options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: "http://localhost:8000",
+  preflightContinue: false,
+};
+
+app.use(cors(cors_options));
+
+// our routes goes here
 app.use(diveRoutes)
 app.use(userRoutes)
+app.use('/auth', CompositionRoot.authRouter())
+// routes end
+
+app.options('*', cors(cors_options));
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
-console.log(uri)
-mongoose.set("useFindAndModify", false)
 
+mongoose.set("useFindAndModify", false)
 mongoose
   .connect(uri, options)
   .then(() =>
